79612166c3cebcd365ba25a5f2fc120c5d21a1b5
eliasmg12345 <randyofgod666@gmail.com>
2023-08-28 16:26:38 -0400

    ui

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { useStore } from './hooks/useStore'
+import { Container, Row, Col, Button, Stack } from 'react-bootstrap'
+import { AUTO_LANGUAGE } from './constants'
+import { ArrowsIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { SectionType } from './types.d'
+import { TextArea } from './components/TextArea'
+
+function App() {
+  const { loading, fromLanguage, toLanguage, fromText, result, interchangeLanguages, setFromLanguage, setToLanguage, setFromText, setResult } = useStore()
+
+  return (
+    <Container fluid>
+      <h1>Google Traslate</h1>
+      <Row>
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage}
+            />
+            <TextArea
+              type={SectionType.From}
+              value={fromText}
+              onChange={setFromText}
+            />
+          </Stack>
+        </Col>
+
+        <Col xs='auto'>
+          <Button
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            onClick={interchangeLanguages}
+          >
+            <ArrowsIcon />
+          </Button>
+        </Col>
+
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage}
+            />
+            <TextArea
+              loading={loading}
+              type={SectionType.To}
+              value={result}
+              onChange={setResult}
+            />
+          </Stack>
+        </Col>
+      </Row>
+      {fromLanguage}
+    </Container>
+  )
+}
+
+export default App
